3fcec46d68140a966739815d329434b583a397bc
NagarajuPeduri <pedurinagaraju7@gmail.com>
2019-03-20 08:19:13 +0530

    tests update

--- Calculator/src/calculatorTest.java
+++ Calculator/src/calculatorTest.java
@@ -1,16 +1,25 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class calculatorTest {
 
+	calculator obj = new calculator();
+	
 	@Test
 	void calculatorTest()		//constructor
 	{
-		calculator obj = new calculator();
+		
 		assertEquals(5, obj.add(3,  2));
 		assertEquals(10, obj.multiply(5,  2));
 		assertEquals(5, obj.modulo(5, 7));
+		Assertions.assertThrows(ArithmeticException.class,()->obj.divide(25, 0));
+	}
+	
+	@Test
+	void test2() {
+		assertEquals(2, obj.divide(10, 5));
 	}
 
 }
